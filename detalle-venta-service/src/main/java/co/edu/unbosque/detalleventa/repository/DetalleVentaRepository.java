a3a1463ef526f20975de645aadf6ec3d8237fefa
Walter Ramos <wilmer32ramos@gmail.com>
2026-03-01 16:52:35 -0500

    wip: add ms-detalleventa
    microservice focused on the details of each sale in the store

---
+++ detalle-venta-service/src/main/java/co/edu/unbosque/detalleventa/repository/DetalleVentaRepository.java
@@ -0,0 +1,15 @@
+package co.edu.unbosque.detalleventa.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import co.edu.unbosque.detalleventa.model.DetalleVenta;
+import java.util.List;
+
+
+public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
+	
+	public Optional<DetalleVenta> findByCodigoDetalleVenta(Long codigoDetalleVenta);
+
+}
